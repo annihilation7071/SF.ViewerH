0536282f01dc747472d2577c40bc3b62002997b7
annihilation7071 <annihilation7071@proton.me>
2025-03-15 20:38:23 +0300

    v186

--- backend/classes/db.py
+++ backend/classes/db.py
@@ -1,3 +1,5 @@
+import os.path
+
 from backend.main_import import *
 from backend.utils import *
 from backend import dep
@@ -6,7 +8,7 @@ log = logger.get_logger("Classes.db")
 
 
 class ProjectBase(SQLModel):
-    _info_path: Path | None = None
+    _path: Path | None = None
     lid: str = Field(..., unique=True, nullable=False)
     info_version: int = Field(..., index=True)
     lvariants: list[str] = Field(default=[], sa_column=Column(JSON, index=True))
@@ -34,30 +36,41 @@ class ProjectBase(SQLModel):
         validate_assignment = True
         allow_arbitrary_types = False
 
+    @property
+    def path(self) -> Path:
+        return self._path
+
     @classmethod
     def project_file_load(cls, file: Path):
         data = utils.read_json(file)
+        data["_path"] = file.parent.parent
         return cls.model_validate(data)
 
     def prolect_file_save(self, file: Path = None, fs: FSession = None):
         if file is None:
-            file = self._info_path
+            file = self.path / "sf.viewer/v_info.json"
         exclude = {
             "id", "dir_name",
             "search_body", "active",
             "lib"
         }
         data = self.model_dump_json(exclude=exclude)
+        file.parent.mkdir(parents=True, exist_ok=True)
         utils.write_json(file, json.loads(data), fs=fs)
 
 
 class Project(ProjectBase, table=True):
     __tablename__ = "projects"
+
     id: int = Field(sa_column=Column("_id", Integer, primary_key=True))
     dir_name: str = Field(default=None, index=True)
     search_body: str = Field(default=None, index=True)
     active: bool = Field(default=True, index=True)
     lib: str = Field(default=None, index=True)
+
+    _images_exts = {".png", ".jpg", ".jpeg", ".gif", ".avif", ".webp",
+                    ".bmp", ".tif", ".tiff", ".apng",
+                    ".mng", ".flif", ".bpg", ".jxl", ".qoi", ".hif"}
 
     @property
     def is_pool(self) -> bool:
@@ -110,12 +123,36 @@ class Project(ProjectBase, table=True):
         return len(self.lvariants)
 
     @classmethod
-    def project_file_load(cls, file: Path):
+    def project_file_load(cls, file: Path) -> None:
         raise IOError("This method allowed only for ProjectBase class.")
 
+    def _project_update_pages(self) -> bool:
+        exts = self._images_exts
+        files = os.listdir(self.path)
+        pages_count = len([file for file in files if os.path.splitext(file)[1].lower() in exts])
+        if pages_count != self.pages:
+            self.pages = pages_count
+            return True
+        return False
 
+    def _project_update_preview(self) -> bool:
+        preview_name = ""
 
+        files = os.listdir(self.path)
 
+        for file in sorted(files):
+            if os.path.splitext(file)[1].lower() in self._images_exts:
+                preview_name = file
+                break
 
+        for file in files:
+            if file.startswith(("preview", "_preview")):
+                preview_name = file
+                break
 
+        if preview_name != self.preview:
+            self.preview = preview_name
+            self.preview_hash = utils.get_imagehash(self.preview_path)
+            return True
+        return False
 
